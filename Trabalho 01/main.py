2a3f9456378797ddcd3040a24de70642dd80c184
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2021-03-30 12:25:30 -0300

    Update main.py

--- Trabalho 01/main.py
+++ Trabalho 01/main.py
@@ -10,44 +10,67 @@
 ### variáveis 
 qtdProvas = 0
 gabarito = ['a','b','a','a','a','a','a','a','a','a']
+matriculas = [1,2,3,4,5,6,7,8,9,0,11,12]
 
 #######################
 ### lendo o arquivo 
 arquivo = open("teste1.txt", "r")
 linhas = arquivo.readlines()
 
-#############################
-### iterando pelo arquivo 
 nLinha = 0
 
+######################################
+### indice do vetor das matriculas 
+index = 0
+
+######################################
+### indice do vetor das matriculas 
 for linha in linhas:
 
     #############################
     ### número da linha atual
     nLinha += 1
-
+    
     ############################################
     ### removendo o caractere ANSI de escape
     x = linha.replace("\n", "")
-    linhaQuebrada = x.split(" ")
     
+    #########################
+    ### separando a linha
+    linhaQuebrada = x.split(" ")
+    tamanho = len(linhaQuebrada)
+
     ########################################################
     ### executando ações de acordo com o número da linha
     if nLinha == 1:
 
         #######################################
         ### preenche a quantidade de provas 
-        qtdProvas = int(input("Digite a quantidade de provas: "))
+        qtdProvas = linhaQuebrada
     
     elif nLinha == 2:
         
         ##############################
         ### preenchendo o gabarito
         i = 0
-        tamanho = len(linhaQuebrada)
         while i < (tamanho):    ##############################
            x = linhaQuebrada[i] ### variável temporária 
            gabarito[i] = x      ### atribuindo ao gabarito
            i += 1
 
+    elif nLinha > 2: ### itera 12 vezes aqui
 
+        print(linhaQuebrada)
+
+        ##################################
+        ### salvando o n° da matrícula
+        x = linhaQuebrada[0] ### variável temporária 
+        matriculas[index] = x ### atribuindo as matrículas
+        index += 1 ### incrementa o indice
+        
+
+print("")
+print(qtdProvas)
+print(gabarito)
+print(matriculas)
+print(nLinha)
