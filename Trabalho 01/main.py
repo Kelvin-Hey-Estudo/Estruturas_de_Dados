7641c013a2fe046c37b69514217232c1f0bd5082
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2021-04-03 21:35:57 -0300

    Update main.py

--- Trabalho 01/main.py
+++ Trabalho 01/main.py
@@ -1,30 +1,22 @@
-# 1 … receber a quantidade de provas a serem corrigidas
-# 2 … receber as alternativas corretas (que podem ser ‘a’, ‘b’, ‘c’, ‘d’ ou ‘e’)
-# 3 … receber as alternativas marcadas por cada um (em ordem)
-# 4 … mostrar o código que identifica o aluno
-# 5 … mostrar o conceito obtido por ele na referida prova
-
-# ! … A prova possui sempre dez questões, sendo que cada uma vale 1 ponto
-
 #################
 ### variáveis 
 qtdProvas = 0
 gabarito = ['a','b','a','a','a','a','a','a','a','a']
-matriculas = [1,2,3,4,5,6,7,8,9,0,11,12]
+matriculas = []
+alternativas = [1,2,3,4,5,6,7,8,9,0]
 
 #######################
 ### lendo o arquivo 
 arquivo = open("teste1.txt", "r")
 linhas = arquivo.readlines()
 
-nLinha = 0
-
 ######################################
 ### indice do vetor das matriculas 
+nLinha = 0
 index = 0
 
-######################################
-### indice do vetor das matriculas 
+#############################
+### iterando pelo arquivo
 for linha in linhas:
 
     #############################
@@ -46,8 +38,8 @@ for linha in linhas:
 
         #######################################
         ### preenche a quantidade de provas 
-        qtdProvas = linhaQuebrada
-    
+        qtdProvas = linhaQuebrada[0]
+        
     elif nLinha == 2:
         
         ##############################
@@ -58,19 +50,47 @@ for linha in linhas:
            gabarito[i] = x      ### atribuindo ao gabarito
            i += 1
 
-    elif nLinha > 2: ### itera 12 vezes aqui
+    elif nLinha > 2: ### itera pelo n° de linhas 
 
-        print(linhaQuebrada)
+        ########################################
+        ### itera pelas respostas dos alunos
+        j = 1
+        for i in range(0, 10):
+            #######################################
+            ### preenche as resposta dos alunos 
+            
+            ### mecanismo de segurança... avançado...
+            ### pula a linha se estiver vazia(ignora)
+            if linhaQuebrada[0] == "":
+                break
+
+            alternativas[i] = linhaQuebrada[j]
+            j += 1
+
+        ################################
+        ### preechendo as matriculas
+        matriculas = []
+        for i in range(0, int(qtdProvas)):
+            matriculas.append(None)
 
         ##################################
         ### salvando o n° da matrícula
-        x = linhaQuebrada[0] ### variável temporária 
+        x = linhaQuebrada[0]  ### variável temporária 
         matriculas[index] = x ### atribuindo as matrículas
-        index += 1 ### incrementa o indice
-        
 
-print("")
-print(qtdProvas)
-print(gabarito)
-print(matriculas)
-print(nLinha)
+        ##########################
+        ### calculando a média
+        total = 0
+        for i in range(0, 10):
+            if gabarito[i] == alternativas[i]:
+                total += 1
+        
+        ##############################
+        ### exibindo os resultados 
+
+        ### mecanismo de segurança... avançado...
+        ### evita que mostre o valor anterior duas vezes
+        if linhaQuebrada[0] != "":
+            print(matriculas[index], total)
+
+            index += 1 ### incrementa o indice
